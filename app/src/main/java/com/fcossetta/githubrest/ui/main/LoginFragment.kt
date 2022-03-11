43ce863b113ffc123b2abb38dbcc88958766bfa1
fcossetta <fcossetta@azinit.it>
2022-03-11 18:15:26 +0100

    basic login impl

--- app/src/main/java/com/fcossetta/githubrest/ui/main/LoginFragment.kt
+++ app/src/main/java/com/fcossetta/githubrest/ui/main/LoginFragment.kt
@@ -1,41 +1,59 @@
 package com.fcossetta.githubrest.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.fcossetta.githubrest.MyApplication
-import com.fcossetta.githubrest.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.fcossetta.githubrest.api.GitHubService
+import com.fcossetta.githubrest.databinding.LoginFragmentBinding
 import com.fcossetta.githubrest.model.AppDatabase
-import dagger.android.AndroidInjection
-import dagger.android.AndroidInjectionKey
 import dagger.android.support.AndroidSupportInjection
 import javax.inject.Inject
 
 class LoginFragment : Fragment() {
 
 
+    private var _binding: LoginFragmentBinding? = null
+
+    private val TAG: String = "AAA"
+
     @Inject
     lateinit var gitHubService: GitHubService
+
     @Inject
     lateinit var appDatabase: AppDatabase
 
     private lateinit var viewModel: LoginViewModel
-    lateinit var myApplication: MyApplication
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+    ): View {
+        _binding = LoginFragmentBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         AndroidSupportInjection.inject(this)
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        binding.loginButton.setOnClickListener {
+            viewModel.login(
+                gitHubService, binding.username.editText!!.text.toString(), binding.passwod
+                    .editText!!.text.toString()
+            ).observe(
+                viewLifecycleOwner
+            ) {
+                Log.e(TAG, "onActivityCreated: $it")
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/fcossetta/githubrest/ui/main/LoginViewModel.kt
+++ app/src/main/java/com/fcossetta/githubrest/ui/main/LoginViewModel.kt
@@ -1,16 +1,23 @@
 package com.fcossetta.githubrest.ui.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.fcossetta.githubrest.api.GitHubService
+import kotlinx.coroutines.launch
 import okhttp3.Credentials
 
 class LoginViewModel : ViewModel() {
 
-    suspend fun login(gitHubService: GitHubService, user: String, pwd: String) {
-        val basic = Credentials.basic(user, pwd)
-        val userToken = gitHubService.getUserToken(basic)
-        if(userToken.isSuccessful){
-
+     fun login(gitHubService: GitHubService, user: String, pwd: String)  = liveData<String> {
+        viewModelScope.launch {
+            val basic = Credentials.basic(user, pwd)
+            val userToken = gitHubService.getUserToken(basic)
+            if(userToken.isSuccessful){
+                emit(userToken.message())
+            }
         }
+
+
     }
 }
